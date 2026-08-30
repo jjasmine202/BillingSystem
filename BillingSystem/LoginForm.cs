e8795e978ebb7d6334149cf9201bbebbbba3530a
jjsace <jjsace@DESKTOP-A7E4Q7D>
2026-08-30 11:25:31 +0800

    Add project files.

---
+++ BillingSystem/LoginForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BillingSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
