747e98822fdf0910fcd2ebbccd8749545db44bba
jjsace <jjsace@DESKTOP-A7E4Q7D>
2026-08-30 19:49:35 +0800

    Add project files.

---
+++ BillingSystem/CustomerListForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace BillingSystem
+{
+    partial class CustomerListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitle = new Label();
+            lblFullName = new Label();
+            lblAddress = new Label();
+            lblContact = new Label();
+            lblEmail = new Label();
+            lblBalance = new Label();
+            txtFullName = new TextBox();
+            txtAddress = new TextBox();
+            txtContact = new TextBox();
+            txtEmail = new TextBox();
+            txtBalance = new TextBox();
+            btnSave = new Button();
+            btnClear = new Button();
+            btnBack = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(0, 0);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(195, 28);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Add New Customer";
+            // 
+            // lblFullName
+            // 
+            lblFullName.AutoSize = true;
+            lblFullName.Location = new Point(12, 40);
+            lblFullName.Name = "lblFullName";
+            lblFullName.Size = new Size(83, 20);
+            lblFullName.TabIndex = 1;
+            lblFullName.Text = "Full Name: ";
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Location = new Point(12, 72);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(65, 20);
+            lblAddress.TabIndex = 2;
+            lblAddress.Text = "Address:";
+            // 
+            // lblContact
+            // 
+            lblContact.AutoSize = true;
+            lblContact.Location = new Point(12, 109);
+            lblContact.Name = "lblContact";
+            lblContact.Size = new Size(121, 20);
+            lblContact.TabIndex = 3;
+            lblContact.Text = "Contact Number:";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(12, 149);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(49, 20);
+            lblEmail.TabIndex = 4;
+            lblEmail.Text = "Email:";
+            // 
+            // lblBalance
+            // 
+            lblBalance.AutoSize = true;
+            lblBalance.Location = new Point(12, 188);
+            lblBalance.Name = "lblBalance";
+            lblBalance.Size = new Size(105, 20);
+            lblBalance.TabIndex = 5;
+            lblBalance.Text = "Initial Balance:";
+            lblBalance.Click += this.label1_Click;
+            // 
+            // txtFullName
+            // 
+            txtFullName.Location = new Point(101, 37);
+            txtFullName.Name = "txtFullName";
+            txtFullName.Size = new Size(125, 27);
+            txtFullName.TabIndex = 6;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(101, 65);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(125, 27);
+            txtAddress.TabIndex = 7;
+            // 
+            // txtContact
+            // 
+            txtContact.Location = new Point(139, 98);
+            txtContact.Name = "txtContact";
+            txtContact.Size = new Size(125, 27);
+            txtContact.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(119, 142);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(125, 27);
+            txtEmail.TabIndex = 9;
+            // 
+            // txtBalance
+            // 
+            txtBalance.Location = new Point(123, 175);
+            txtBalance.Name = "txtBalance";
+            txtBalance.Size = new Size(125, 27);
+            txtBalance.TabIndex = 10;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(54, 289);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 11;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(170, 289);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 29);
+            btnClear.TabIndex = 12;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(282, 289);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(94, 29);
+            btnBack.TabIndex = 13;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            // 
+            // CustomerListForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(432, 373);
+            Controls.Add(btnBack);
+            Controls.Add(btnClear);
+            Controls.Add(btnSave);
+            Controls.Add(txtBalance);
+            Controls.Add(txtEmail);
+            Controls.Add(txtContact);
+            Controls.Add(txtAddress);
+            Controls.Add(txtFullName);
+            Controls.Add(lblBalance);
+            Controls.Add(lblEmail);
+            Controls.Add(lblContact);
+            Controls.Add(lblAddress);
+            Controls.Add(lblFullName);
+            Controls.Add(lblTitle);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "CustomerListForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Billing System - Add Customer";
+            Load += this.CustomerListForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitle;
+        private Label lblFullName;
+        private Label lblAddress;
+        private Label lblContact;
+        private Label lblEmail;
+        private Label lblBalance;
+        private TextBox txtFullName;
+        private TextBox txtAddress;
+        private TextBox txtContact;
+        private TextBox txtEmail;
+        private TextBox txtBalance;
+        private Button btnSave;
+        private Button btnClear;
+        private Button btnBack;
+    }
+}
---
+++ BillingSystem/CustomerListForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BillingSystem
+{
+    public partial class CustomerListForm : Form
+    {
+        public CustomerListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomerListForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
